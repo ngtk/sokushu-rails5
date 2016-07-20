f71d36d37cff59d2c58ca39fcf0e1506cde00a06
Kent Nagata <ngtknt@me.com>
2016-07-20 17:53:33 +0900

    `rails db:migrate`

--- normal/db/schema.rb
+++ normal/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720084156) do
+ActiveRecord::Schema.define(version: 20160720085235) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20160720084156) do
     t.decimal  "price_in_cents"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.boolean  "verified"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
